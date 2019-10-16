1f8ac79bb5c43021bfc929b125e2c9972b520cb4
Aleksandr Reva <reva-alexander@yandex.ru>
2019-10-16 18:57:20 +0300

    infrastructure works

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
+const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
     entry: {
@@ -14,6 +15,9 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'pages/[name]/[name].[chunkhash].js'
     },
+    // devServer: { 
+    //     contentBase: 'pages/index', 
+    // }, 
     module: {
         rules: [
             {
@@ -21,9 +25,23 @@ module.exports = {
             use: { loader: "babel-loader" }, 
             exclude: /node_modules/ 
             },
+            // {
+            // test: /\.css$/i,
+            // use:  [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+            // },
             {
-            test: /\.css$/i,
-            use:  [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+                test: /\.css$/i,
+                use: [
+                    (isDev ? 'style-loader' :
+                    {
+                    loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../../',
+                        }
+                    }),
+                    'css-loader',
+                    'postcss-loader'
+                ]
             },
             {
             test: /\.(gif|png|jpe?g|svg)$/i,
@@ -55,26 +73,27 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: 'pages/[name]/[name].[chunkhash].css'
         }),
+        // new HtmlWebpackPlugin(),
         new HtmlWebpackPlugin({
             inject: false,
             hash: true,
             template: './src/pages/index/index.html',
-            chunks: ['index'],
-            filename: 'pages/index/index.html'
+            filename: 'index.html'
         }),
         new HtmlWebpackPlugin({
             inject: false,
             hash: true,
             template: './src/pages/about/about.html',
-            chunks: ['about'],
-            filename: 'pages/about/about.html'
+            filename: 'about.html'
         }),
         new HtmlWebpackPlugin({
             inject: false,
             hash: true,
             template: './src/pages/news/news.html',
-            chunks: ['news'],
-            filename: 'pages/news/news.html'
+            filename: 'news.html'
+        }),
+        new webpack.DefinePlugin({
+            'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
         })
     ]
 };
